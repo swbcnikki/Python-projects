fb895b9595cfe8340450c7f7003b8c1be18c3ff2
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-04-07 22:42:48 -0700

    Update dBinPy01.py

--- dBinPy01.py
+++ dBinPy01.py
@@ -6,7 +6,7 @@ conn = sqlite3.connect('test.db')
 with conn:
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_persons( \
-    ID INTEGER PRIMARY KEY AUTOINCREMENT. \
+    ID INTEGER PRIMARY KEY AUTOINCREMENT, \
     col_fname TEXT, \
     col_lname TEXT, \
     col_email TEXT \
