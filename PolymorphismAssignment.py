1b9ccf73f65a176ba224b57f10f469ad1777939b
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-04-09 17:55:45 -0700

    updating

--- PolymorphismAssignment.py
+++ PolymorphismAssignment.py
@@ -1,51 +1,53 @@
 
 
+
+
 #Parent Class StarTrek TOS
 class StarTrekBlue:
-    creator = 'Gene Roddenberry'
-    TOS_setting = 'Starship'
-    TOS_captain = 'James T Kirk'
-    TOS_vessel = 'USS Enterprise'
+    creator = 'Gene Roddenberry' # attributes
+    setting = 'Starship'
+    captain = 'James T Kirk'
+    vessel = 'USS Enterprise'
 
-    def getTickets(self):
+    def getTickets(self): # method for parent class indented under it
         enter_creator = input('Name of creator: ')
-        enter_TOS_setting = input('TOS Environment: ')
-        enter_TOS_captain = input('Name of TOS captain: ')
-        enter_TOS_vessel = input('Name of TOS vessel: ')
-        if (enter_creator == self.creator and enter_TOS_setting == self.TOS_setting and
-            enter_TOS_captain == self.TOS_captain and enter_TOS_vessel == self.TOS_vessel):
+        enter_setting = input('TOS Environment: ')
+        enter_captain = input('Name of TOS captain: ')
+        enter_vessel = input('Name of TOS vessel: ')
+        if (enter_creator == self.creator and enter_setting == self.setting and
+            enter_captain == self.captain and enter_vessel == self.vessel):
             print('\nYou win tickets to TrekkieCon level 1!\n')
         else:
             print('\nNo entry! Review your answers and try again!\n')
 
             
 #Child Class StarTrek TNG
-class StarTrekRed(StarTrekBlue):
-    TNG_captain = 'Jean-Luc Picard'
-    TNG_vessel = 'USS Enterprise D'
-    TNG_security_chief = 'Worf son of Mogh'
+class StarTrekRed(StarTrekBlue): #connects parent and this child class
+    captain = 'Jean-Luc Picard' # captain name override
+    vessel = 'USS Enterprise D' # vessel name override
+    security_chief = 'Worf son of Mogh' # new attribute for this child
 
-    def getTickets(self):
-        enter_TNG_captain = input('Name of TNG captain: ')
-        enter_TNG_vessel = input('Name of TNG vessel: ')
-        enter_TNG_security_chief = input('Full name of TNG Chief of Security: ')
-        if (enter_TNG_captain == self.TNG_captain and enter_TNG_vessel == self.TNG_vessel and enter_TNG_security_chief == self.TNG_security_chief):
+    def getTickets(self): # method for this child class indented under it
+        enter_captain = input('Name of TNG captain: ')
+        enter_vessel = input('Name of TNG vessel: ')
+        enter_security_chief = input('Full name of TNG Chief of Security: ')
+        if (enter_captain == self.captain and enter_vessel == self.vessel and enter_security_chief == self.security_chief):
             print('\nYou win tickets to TrekkieCon level 2!\n')
         else:
             print('\nNo entry! Review your answers and try again!\n')
 
 
 #Child Class StarTrek DS9
-class StarTrekYellow(StarTrekBlue):
-    DS9_captain = 'Benjamin Sisko'
-    DS9_vessel = 'USS Defiant'
-    DS9_setting = 'Space Station'
+class StarTrekYellow(StarTrekBlue): # connects parent and this child class
+    captain = 'Benjamin Sisko' # captain name override
+    vessel = 'USS Defiant' # vessel name override
+    setting = 'Space Station' # setting override
 
-    def getTickets(self):
-        enter_DS9_captain = input('Name of DS9 captain: ')
-        enter_DS9_vessel = input('Name of DS9 vessel: ')
-        enter_DS9_setting = input('DS9 Environment: ')
-        if (enter_DS9_captain == self.DS9_captain and enter_DS9_vessel == self.DS9_vessel and enter_DS9_setting == self.DS9_setting):
+    def getTickets(self): # method for this child class indented under it
+        enter_captain = input('Name of DS9 captain: ')
+        enter_vessel = input('Name of DS9 vessel: ')
+        enter_setting = input('DS9 Environment: ')
+        if (enter_captain == self.captain and enter_vessel == self.vessel and enter_setting == self.setting):
             print('\nYou win tickets to TrekkieCon level 3!\n')
         else:
             print('\nNo entry! Review your answers and try again!\n')            
