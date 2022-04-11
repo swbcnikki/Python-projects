981ca5fc51b0bb3f4cf6e440cbad537dd45c4121
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-04-11 12:44:21 -0700

    Update encapsulation.py

--- encapsulation.py
+++ encapsulation.py
@@ -4,24 +4,24 @@ class Password:
     def __init__(self):
         self.passwordVar = 'qwerty1' #This is a non protected password
                 
-        self._passwordVar = 'qwerty123' #This is a protected password because of the _ before the word password
+        self._passwordsVar = 'qwerty123' #This is a protected password because of the _ before the word password
                 
-        self.__passwordVar = 'qwerty123!@#' #This is a private password because of the __ before the word password
+        self.__passwordssVar = 'qwerty123!@#' #This is a private password because of the __ before the word password
 
     def getPassword(self):
-        print(self.__passwordVar)
+        print(self.__passwordssVar)
 
-    def setPassword(self):
-        self.__passwordVar = password
+    def setPassword(self, password):
+        self.__passwordssVar = password
         
 
 obj = Password()
 
 print(obj.passwordVar)
 
-print(obj._passwordVar)
+print(obj._passwordsVar)
 
 obj.getPassword()
-obj.setPassword()
-print(obj.password)
+obj.setPassword('zxcvb')
+obj.getPassword()
 
