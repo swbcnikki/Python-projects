a722a5903f81d6688bf41a0e20851ae2ab1dcff4
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-04-12 14:18:18 -0700

    Update abstraction.py

--- abstraction.py
+++ abstraction.py
@@ -1,20 +1,20 @@
 
 
-from abc import ABC, abstractmethod
-class secretAgentMan(ABC):
-    def realName(self, name):
-        print('Real name of agent: ', name)
+from abc import ABC, abstractmethod #importing abc module
+class secretAgentMan(ABC): #abstract class
+    def realName(self,name): #method - regular method
+        print('Real name of agent: ',name)
 
     @abstractmethod
-    def moniker(self, name):
+    def moniker(self): #method definition
         pass
 
 class Alias(secretAgentMan):
-    def moniker(self, name):
+    def moniker(self): #abstract method definition
         print('Your undercover name will be {} for this case '.format('Napoleon'))
 
 
 obj = Alias()
 obj.realName('Chad')
-obj.Alias('Napoleon')
-obj.Alias()
+obj.moniker()
+
